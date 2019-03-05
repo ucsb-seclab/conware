96daf5f63d4dd2de7a1fcd8ca51eb4e37624954d
Aravind Machiry <machiry@cs.ucsb.edu>
2019-03-05 15:34:33 -0800

    Towards complete instrumentation

--- llvm_build_infra/llvm_build/arduino_llvm_build.py
+++ llvm_build_infra/llvm_build/arduino_llvm_build.py
@@ -26,6 +26,15 @@ def setup_args():
     required_named.add_argument('-clangp', action='store', dest='clang_path',
                                 help='Absolute path to the clang binary')
 
+    required_named.add_argument('-instrument', action='store_true', dest='do_instrumentation',
+                                help='Flag to indicate to perform complete instrumentation.')
+
+    required_named.add_argument('-optp', action='store', dest='opt_path',
+                                help='Absolute path to the opt binary')
+
+    required_named.add_argument('-sopath', action='store', dest='llvm_pass_so_path',
+                                help='Absolute path to LLVM Transformation Pass so file.')
+
     return parser
 
 
@@ -33,9 +42,20 @@ def main():
     arg_parser = setup_args()
     parsed_args = arg_parser.parse_args()
     clang_path = parsed_args.clang_path
+    opt_path = parsed_args.opt_path
+    llvm_pass_so_path = parsed_args.llvm_pass_so_path
     if not os.path.exists(clang_path):
         log_error("clang not available in the system path:", clang_path)
         sys.exit(-1)
+
+    if parsed_args.do_instrumentation is True:
+        if not os.path.exists(opt_path):
+            log_error("opt not available in the system path:", opt_path)
+            sys.exit(-1)
+
+        if not os.path.exists(llvm_pass_so_path):
+            log_error("LLVM so path not available in the system path:", llvm_pass_so_path)
+            sys.exit(-1)
     # get all the compilation commands.
     compile_commands = parse_compile_json(parsed_args.compile_json)
     os.system("mkdir -p " + parsed_args.llvm_bc_out)
