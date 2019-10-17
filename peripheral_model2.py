a66f46dc52219797374618f0b91d789bf11bde15
Graham Foster <gmfoster@ucsb.edu>
2019-10-17 14:31:34 -0700

    Added new periphral model and state classes, which from what I can tell produce the correct model,  networkx visualize currently only visualizes the UART

--- peripheral_model2.py
+++ peripheral_model2.py
@@ -6,37 +6,42 @@ from peripheral_state2 import PeripheralModelState2
 
 logger = logging.getLogger(__name__)
 
+
 class PeripheralModel2:
     """
     This class represents an external peripheral
     """
 
-    nodeID = 0
+    global_nodeID = 0
 
     def __init__(self, addresses, name=None):
+        self.all_states = []
+        self.nodeID = 0
         self.graph = networkx.DiGraph()  # May want to use MultiDiGraph instead, allows parallel edges
         self.addresses = addresses
         self.name = name
-        self.current_state = self.create_state(-1, "start", 0)
-
+        self.current_state = self.create_state(-1, "start", -1)
+        self.start_state = self.current_state
 
     def update_node_id(self):
-        PeripheralModel2.nodeID += 1
+        self.nodeID += 1
 
     def create_state(self, address, operation, value):
-        #create state attributes
-        state_id = PeripheralModel2.nodeID
+        # create state attributes
+        # state_id = PeripheralModel2.nodeID
+        state_id = (self.nodeID, PeripheralModel2.global_nodeID)
+        PeripheralModel2.global_nodeID += 1
         state = PeripheralModelState2(address, operation, value)
         attributes = {state_id: state.reads}
-        #create state
+        # create state
         # check for state existance?
         if not self.graph.has_node(state_id):
             self.graph.add_node(state_id)
 
         networkx.set_node_attributes(self.graph, attributes)
+
+        self.all_states.append(state)
         return state_id, state
-
-
 
     def train_read(self, address, value, pc, size, timestamp):
         """
@@ -50,9 +55,20 @@ class PeripheralModel2:
     def train_write(self, address, value):
         self.update_node_id()
         new_state = self.create_state(address, "write", value)
+        old_state = self.current_state
         self.current_state = new_state
-        self.graph.add_edge(self.current_state[0]-1, self.current_state[0])
+        # self.graph.add_edge(self.current_state[0]-1, self.current_state[0], value=value)
+        self.graph.add_edge(old_state[0], self.current_state[0], value=value)
 
-        ###TODO####
-        ##Add Edge between two states###
-        ##State moving from will always be the state the edge comes from###
+    def train(self):
+        for idx, state in enumerate(self.all_states):
+            state.train()
+
+    def reset(self):
+        self.current_state = self.start_state
+
+        for state in self.all_states:
+            state.reset()
+
+
+
